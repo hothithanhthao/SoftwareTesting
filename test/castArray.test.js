653089f82adbe014df848a77e5f53e6f9f9003da
Thao Ho <thao.ho@tuni.fi>
2021-11-21 01:27:56 +0200

    Create TCs for castArray, clamp, compact, isBoolean and upperFirst

--- test/castArray.test.js
+++ test/castArray.test.js
@@ -1,12 +1,51 @@
 import castArray from "../src/castArray";
-//const castArray = require('../src/castArray');
 
-describe("Calculator tests", () => {
-    test('adding 1 + 2 should return 3', () => {
-      // arrange and act
-      var result = castArray(true);
-    
-      // assert
-      expect(result).toEqual([true]);
+describe("castArray", () => {
+    test("should cast castArray if the input is boolean", () => {
+      const input = true;
+      const output = [true];
+
+      expect(castArray(input)).toEqual(output);
+    });
+
+    test("should cast castArray if the input is number", () => {
+      const input = 8;
+      const output = [8];
+
+      expect(castArray(input)).toEqual(output);
+    });
+
+    test("should cast castArray if the input is object", () => {
+      const input = { 'a': 1 };
+      const output = [{ 'a': 1 }];
+
+      expect(castArray(input)).toEqual(output);
+    });
+
+    test("should cast castArray if the input is null", () => {
+      const input = null;
+      const output = [null];
+
+      expect(castArray(input)).toEqual(output);
+    });
+
+    test("should cast castArray if the input is string", () => {
+      const input = "Hello Word";
+      const output = ["Hello Word"];
+
+      expect(castArray(input)).toEqual(output);
+    });
+
+    test("should cast castArray if the input is empty - TC failed", () => {
+      const output = [];
+
+      expect(castArray()).toEqual(output);
+    });
+
+    test("should not cast castArray if the input is array", () => {
+      const input = [ 8, 9, true ];
+      const output = [ 8, 9, true ];
+      
+      expect(castArray(input)).toEqual(output);
     });
 });
