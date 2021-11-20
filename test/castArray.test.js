529ec78015765b1daf25beae1df6c58d0c78c3b2
Thao Ho <thao.ho@tuni.fi>
2021-11-20 22:01:44 +0200

    Set up jest and c8

---
+++ test/castArray.test.js
@@ -0,0 +1,12 @@
+import castArray from "../src/castArray";
+//const castArray = require('../src/castArray');
+
+describe("Calculator tests", () => {
+    test('adding 1 + 2 should return 3', () => {
+      // arrange and act
+      var result = castArray(true);
+    
+      // assert
+      expect(result).toEqual([true]);
+    });
+});
