dc0df3bef9b73d2976cf7aa5a7bf6a890ef84f67
Nhan Nguyen <94371134+nhannguyen0405@users.noreply.github.com>
2021-12-04 16:45:28 +0200

    Fix expected return from Filter.js function

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,23 +1,23 @@
 import filter from "../src/filter";
 
 describe("filter", () => {
-  test.skip("should return barney object when filter by active == true", () => {
+  test("should return barney object when filter by active == true", () => {
     const users = [
       { user: "barney", active: true },
       { user: "fred", active: false },
     ];
     const predicate = ({ active }) => active
-    const output = { user: "barney", active: true }
+    const output = [{ user: "barney", active: true }]
     expect(filter(users, predicate)).toEqual(output);
   });
 
-  test.skip("should return fred object when filter by active == false", () => {
+  test("should return fred object when filter by active == false", () => {
     const users = [
       { user: "barney", active: true },
       { user: "fred", active: false },
     ];
     const predicate = ({ active }) => !active
-    const output = { user: "fred", active: false }
+    const output = [{ user: "fred", active: false }]
     expect(filter(users, predicate)).toEqual(output);
   });
 });
